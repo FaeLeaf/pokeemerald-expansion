299673fb7d3b6e4affd5b25e7857de9661362501
AgustinGDLV <agustingomezdelavega@gmail.com>
2025-05-21 11:18:38 -0700

    added GBC font

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -272,6 +272,9 @@ $(FONTGFXDIR)/frlg_male.fwjpnfont: $(FONTGFXDIR)/japanese_frlg_male.png
 $(FONTGFXDIR)/frlg_female.fwjpnfont: $(FONTGFXDIR)/japanese_frlg_female.png
 	$(GFX) $< $@
 
+$(FONTGFXDIR)/gbc.latfont: $(FONTGFXDIR)/gbc.png
+	$(GFX) $< $@
+
 
 ### Miscellaneous ###
 
